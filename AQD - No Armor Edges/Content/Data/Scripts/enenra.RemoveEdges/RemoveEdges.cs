7c5d50eef2a0fda1fe900974c58c8d31a41a0966
enenra <enenra9@gmail.com>
2019-11-29 16:15:06 +0100

    Changed the way the mod works fundamentally
    Changed the functionality over from replacing the edges with "empty" models (which resulted in shadows being cast from the invisbile models) to a script which dynamically removes edges on all armor blocks. This doesn't change anything in the effect of the mod, but will hopefully alleviate some of its quirks.
    
    Fixes #3 .

---
+++ AQD - No Armor Edges/Content/Data/Scripts/enenra.RemoveEdges/RemoveEdges.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace enenra.RemoveEdges
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class RemoveEdges : MySessionComponentBase
+    {
+        private List<MyCubeBlockDefinition> revertShowEdges = new List<MyCubeBlockDefinition>();
+
+        public override void LoadData()
+        {
+            foreach (MyDefinitionBase def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+                MyCubeBlockDefinition blockDef = def as MyCubeBlockDefinition;
+
+                if (blockDef == null)
+                    continue;
+
+                if (blockDef.BlockTopology == MyBlockTopology.TriangleMesh)
+                    continue;
+
+                if (!blockDef.CubeDefinition.ShowEdges)
+                    continue;
+
+                blockDef.CubeDefinition.ShowEdges = false;
+                revertShowEdges.Add(blockDef);
+            }
+        }
+
+        protected override void UnloadData()
+        {
+            foreach (var def in revertShowEdges)
+            {
+                def.CubeDefinition.ShowEdges = true;
+            }
+
+            revertShowEdges.Clear();
+        }
+    }
+}
