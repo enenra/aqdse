3d89d327e93d2514b0e78355702c600dbfa24b58
enenra <enenra9@gmail.com>
2019-12-15 00:14:24 +0100

    Rework of QoL
    * added message to player about changes to dependencies (code by SISK)
    * moved camera adjustments to script (with lots of help from Thraxus)
    * moved ammo adjustments to script (with lots of help from Thraxus)
    * split up and moved CubeBlocks
    * added light range adjustments for normal lights
    * increased spotlight range further

---
+++ AQD - Quality of Life/Content/Data/Scripts/enenra.QoL/LoadMessage.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game.Components;
+
+// This code was written for me by SISK - many thanks to him!
+namespace enenra.QoL
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class LoadMessage : MySessionComponentBase
+    {
+        private const string SENDER = "enenra";
+        private const string VARIABLE_NAME = "displayCount";
+        private const int MAX_TIMES = 2;
+        private const string MESSAGE1 = "PSA for AQD - Quality of Life mod users: I've decided to refocus the mod. The following mods have been removed from the pack: BuildVision, HUD Compass, Sleep Mod. They will be moved to another modpack instead. Please visit the QoL mod page for more details.";
+        private const string MESSAGE2 = "This message will only show twice on load per world, and will be removed completely in a week or so.";
+        private int _amountDisplayed;
+
+        public override void LoadData()
+        {
+            MyAPIGateway.Utilities.GetVariable(VARIABLE_NAME, out _amountDisplayed);
+            MyAPIGateway.Session.OnSessionReady += OnSessionReady;
+        }
+
+        protected override void UnloadData()
+        {
+            MyAPIGateway.Session.OnSessionReady -= OnSessionReady;
+        }
+
+        private void OnSessionReady()
+        {
+            MyAPIGateway.Session.OnSessionReady -= OnSessionReady;
+            if (_amountDisplayed < MAX_TIMES)
+            {
+                _amountDisplayed++;
+                MyAPIGateway.Utilities.ShowMessage(SENDER, MESSAGE1);
+                MyAPIGateway.Utilities.ShowMessage(SENDER, MESSAGE2);
+                MyAPIGateway.Utilities.SetVariable(VARIABLE_NAME, _amountDisplayed);
+            }
+        }
+    }
+}
---
+++ AQD - Quality of Life/Content/Data/Scripts/enenra.QoL/QoLAdjustments.cs
@@ -0,0 +1,78 @@
+﻿using VRage.Game.Components;
+using Sandbox.Definitions;
+using VRage.Game;
+using System.Linq;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+
+namespace enenra.QoL
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class QoLAdjustments : MySessionComponentBase
+    {
+        public override void BeforeStart()
+        {
+            base.BeforeStart();
+            if (MyAPIGateway.Multiplayer.IsServer) return;
+            ChangeCubeBlocks();
+            ChangeAmmos();
+        }
+
+        /*
+        public override void UpdateBeforeSimulation()
+        {
+            base.UpdateBeforeSimulation();
+            // place method calls here
+        }
+        */
+
+        private void ChangeCubeBlocks()
+        {
+            var camTexturePath = @"\Textures\GUI\Screens\camera_overlay.dds";
+            var turretTexturePath = @"\Textures\GUI\Screens\turret_overlay.dds";
+            var camTextureFullPath = ModContext.ModPath + camTexturePath;
+            var turretTextureFullPath = ModContext.ModPath + turretTexturePath;
+
+            foreach (MyCubeBlockDefinition myCubeBlockDefinition in MyDefinitionManager.Static.GetAllDefinitions().Select(myDefinitionBase => myDefinitionBase as MyCubeBlockDefinition).Where(myCubeBlockDefinition => myCubeBlockDefinition?.Components != null))
+            {
+                if (myCubeBlockDefinition.Id.TypeId == typeof(MyObjectBuilder_CameraBlock))
+                {
+                    var camDef = myCubeBlockDefinition as MyCameraBlockDefinition;
+                    camDef.OverlayTexture = camTextureFullPath;
+                }
+                else if (myCubeBlockDefinition.Id.TypeId == typeof(MyObjectBuilder_InteriorTurret) || myCubeBlockDefinition.Id.TypeId == typeof(MyObjectBuilder_LargeGatlingTurret) || myCubeBlockDefinition.Id.TypeId == typeof(MyObjectBuilder_LargeMissileTurret))
+                {
+                    var turretDef = myCubeBlockDefinition as MyLargeTurretBaseDefinition;
+                    turretDef.OverlayTexture = turretTextureFullPath;
+                }
+            }
+        }
+
+        private static void ChangeAmmos()
+        {
+            foreach (MyDefinitionBase def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+                MyAmmoDefinition ammoDef = def as MyAmmoDefinition;
+                if (ammoDef == null) continue;
+
+                if (ammoDef.Id.TypeId == typeof(MyProjectileAmmoDefinition))
+                {
+                    var caliberDef = ammoDef as MyProjectileAmmoDefinition;
+
+                    if (caliberDef.Id.SubtypeName == "SmallCaliber")
+                    {
+                        caliberDef.ProjectileTrailColor.X = 0.25f;
+                        caliberDef.ProjectileTrailColor.Y = 0.125f;
+                        caliberDef.ProjectileTrailColor.Z = 0.1f;
+                    }
+                    else if (caliberDef.Id.SubtypeName == "LargeCaliber")
+                    {
+                        caliberDef.ProjectileTrailColor.X = 0.25f;
+                        caliberDef.ProjectileTrailColor.Y = 0.125f;
+                        caliberDef.ProjectileTrailColor.Z = 0.1f;
+                    }
+                }
+            }
+        }
+    }
+}
