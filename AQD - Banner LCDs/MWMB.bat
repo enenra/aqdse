ed97c96a2163723d94b252a84b0d86eaa22dfde1
enenra <enenra9@gmail.com>
2019-12-21 20:07:25 +0100

    Reorganized my model raw files
    (raw files are not synced)

---
+++ AQD - Banner LCDs/MWMB.bat
@@ -0,0 +1,2 @@
+"E:\Games\Modding\SE\tools\MwmBuilder\MwmBuilder.exe" /f /s:.\OriginalContent\ /o:.\Content\ /x:"C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineersModSDK\OriginalContent\Materials"
+PAUSE
