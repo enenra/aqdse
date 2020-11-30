f79c4ae8ea3394a1899011c19c9bd516a04f0718
enenra <enenra9@gmail.com>
2020-11-30 13:07:43 +0100

    Research & Vanilla Tech Tree: Now in beta state.

---
+++ AQD - Research/Content/Data/Scripts/Research/JTurp/ResearchGroupSettings.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+using VRage.Game;
+using VRage.ObjectBuilders;
+
+namespace AQDResearch
+{
+
+  public class SerialId
+  {
+    [XmlAttribute("Type")]
+    public string TypeId;
+
+    [XmlAttribute("SubtypeId")]
+    public string SubtypeId;
+
+    [XmlIgnore]
+    MyDefinitionId _definitionId = new MyDefinitionId();
+
+    public SerialId() { }
+
+    public SerialId(SerializableDefinitionId id)
+    {
+      bool isNull = id.IsNull();
+      TypeId = isNull ? "MyObjectBuilder_TypeGoesHere" : id.TypeIdString;
+      SubtypeId = isNull ? "SubtypeGoesHere" : id.SubtypeId;
+    }
+
+    public MyDefinitionId DefinitionId
+    {
+      get
+      {
+        if (_definitionId.TypeId.IsNull)
+        {
+          MyObjectBuilderType typeId;
+          if (!MyObjectBuilderType.TryParse(TypeId, out typeId))
+            throw new Exception($"Incorrect TypeId given for Definition: '{TypeId}'");
+
+          _definitionId = new MyDefinitionId(typeId, SubtypeId);
+        }
+
+        return _definitionId;
+      }
+    }
+  }
+
+  public class ResearchGroup
+  {
+    [XmlElement("Item")]
+    public SerialId ComponentId;
+
+    [XmlArrayItem("Id")]
+    public List<SerialId> BlockDefinitons { get; set; } = new List<SerialId>();
+
+    public ResearchGroup() { }
+
+    public ResearchGroup(SerializableDefinitionId comp, List<SerializableDefinitionId> defs)
+    {
+      ComponentId = new SerialId(comp);
+
+      if (defs?.Count > 0)
+      {
+        foreach (var item in defs)
+          BlockDefinitons.Add(new SerialId(item));
+      }
+    }
+  }
+
+  public class ResearchGroupSettings
+  {
+    public bool AutoAdjustAssemblerEfficiency { get; set; } = true;
+
+    [XmlElement("ResearchGroup", typeof(ResearchGroup))]
+    public List<ResearchGroup> ResearchGroupList { get; set; } = new List<ResearchGroup>();
+
+    [XmlIgnore]
+    public Dictionary<SerializableDefinitionId, ResearchGroup> ResearchGroups { get; set; } = new Dictionary<SerializableDefinitionId, ResearchGroup>();
+
+    public void AddResearchGroup(ResearchGroup group)
+    {
+      MyObjectBuilderType typeId;
+      if (!MyObjectBuilderType.TryParse(group.ComponentId.TypeId, out typeId))
+        return;
+
+      var compDef = new MyDefinitionId(typeId, group.ComponentId.SubtypeId);
+      ResearchGroups[compDef] = group;
+      ResearchGroupList.Add(group);
+    }
+
+    public void AddResearchGroup(SerializableDefinitionId compDef, List<SerializableDefinitionId> defList)
+    {
+      var group = new ResearchGroup(compDef, defList);
+      ResearchGroups[compDef] = group;
+      ResearchGroupList.Add(group);
+    }
+
+    public void Close()
+    {
+      if (ResearchGroups != null)
+      {
+        foreach (var item in ResearchGroups.Values)
+          item?.BlockDefinitons?.Clear();
+      }
+
+      if (ResearchGroupList != null)
+      {
+        foreach (var item in ResearchGroupList)
+          item?.BlockDefinitons?.Clear();
+      }
+
+      ResearchGroups?.Clear();
+      ResearchGroupList?.Clear();
+      ResearchGroups = null;
+      ResearchGroupList = null;
+    }
+  }
+}
---
+++ AQD - Vanilla Tech Tree/Content/Data/Scripts/Research/JTurp/Logger.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Sandbox.ModAPI;
+
+namespace AQDResearch
+{
+  public class Logger
+  {
+    StringBuilder _builder;
+    TextWriter _writer;
+    bool _isClosed;
+
+    public Logger(string filename, bool isDedicated)
+    {
+      _writer = isDedicated ? MyAPIGateway.Utilities.WriteFileInLocalStorage(filename, typeof(Logger)) : MyAPIGateway.Utilities.WriteFileInWorldStorage(filename, typeof(Logger));
+      _builder = new StringBuilder(1024);
+      _isClosed = false;
+    }
+
+    string DateTimeNow => DateTime.Now.ToString("[HH:mm:ss]");
+
+    public void AddDebug(string text)
+    {
+      _builder.Append(text);
+    }
+
+    public void Log(MessageType msgType = MessageType.DEBUG)
+    {
+      if (_isClosed || _builder.Length == 0)
+        return;
+
+      Log(string.Empty, msgType);
+    }
+
+    public void Log(string text, MessageType msgType = MessageType.DEBUG)
+    {
+      if (_isClosed)
+        return;
+
+      if (string.IsNullOrEmpty(text))
+      {
+        if (_builder.Length == 0)
+          return;
+
+        text = _builder.ToString();
+        _builder.Clear();
+      }
+
+      _writer?.WriteLine($"{DateTimeNow} {msgType} | {text}");
+
+      if (_builder.Length > 0)
+      {
+        _writer?.WriteLine(_builder.ToString());
+        _builder.Clear();
+      }
+
+      _writer?.Flush();
+    }
+
+    public void Log(StringBuilder text, MessageType msgType = MessageType.DEBUG)
+    {
+      if (!_isClosed)
+        Log(text.ToString(), msgType);
+    }
+
+    public void Close()
+    {
+      if (!_isClosed)
+      {
+        _writer?.Flush();
+        _writer?.Close();
+        _isClosed = true;
+      }
+    }
+
+    public static void WriteData(StringBuilder data, string filename)
+    {
+      using (TextWriter writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(filename, typeof(Logger)))
+      {
+        writer.Write(data);
+        writer.Flush();
+      }
+    }
+  }
+
+  public enum MessageType { ERROR, WARNING, DEBUG }
+}
