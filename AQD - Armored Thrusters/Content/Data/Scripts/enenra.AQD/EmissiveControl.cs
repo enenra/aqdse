d76e3e8425032aaeea5b6cecf341be77c821a6ac
enenra <enenra9@gmail.com>
2020-12-01 23:50:21 +0100

    Armored Thrusters: Fixes by jTurp, added support for emissive recolors by other mods

--- AQD - Armored Thrusters/Content/Data/Scripts/enenra.AQD/EmissiveControl.cs
+++ AQD - Armored Thrusters/Content/Data/Scripts/enenra.AQD/EmissiveControl.cs
@@ -7,58 +7,81 @@ using Sandbox.Common.ObjectBuilders;
 
 namespace enenra.EmissiveControl
 {
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), false)]
+  [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), false)]
 
-    public class Container : MyGameLogicComponent
+  public class Container : MyGameLogicComponent
+  {
+    IMyThrust m_block;
+
+    private const string EMISSIVE_MATERIAL_NAME = "Emissive";
+    private Color GREEN = new Color(0, 255, 0);
+    private Color RED = new Color(255, 0, 0);
+
+    bool IsWorking => m_block.IsWorking;
+
+    public override void Init(MyObjectBuilder_EntityBase objectBuilder)
     {
-        IMyThrust m_block;
+      m_block = Entity as IMyThrust;
 
-        private string EMISSIVE_MATERIAL_NAME = "Emissive";
-        private Color GREEN = new Color(0, 255, 0);
-        private Color RED = new Color(255, 0, 0);
-        private Color lastColor;
+      var subtype = m_block.BlockDefinition.SubtypeName;
+      if (!subtype.StartsWith("AQD_LG_") && !subtype.StartsWith("AQD_SG_"))
+      {
+        m_block = null;
+        return;
+      }
 
+      NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
 
-        bool IsWorking
-        {
-            get
-            {
-                return m_block.IsWorking;
-            }
-        }
-
-        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-        {
-            NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
-
-            m_block = Entity as IMyThrust;
-        }
-
-        public override void Close()
-        {
-            m_block = null;
-        }
-
-        public override void UpdateAfterSimulation10()
-        {
-            if (MyAPIGateway.Session == null)
-                return;
-
-            if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
-                return;
-
-            if (IsWorking)
-            {
-                m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, GREEN, 1f);
-                lastColor = GREEN;
-            }
-
-            else
-            {
-                m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, RED, 1f);
-                lastColor = RED;
-            }
-
-        }
+      m_block.IsWorkingChanged += OnIsWorkingChanged;
+      base.Init(objectBuilder);
     }
+
+    public override void UpdateOnceBeforeFrame()
+    {
+      bool aqdVisualsPresent = false;
+      bool emissiveColorsPresent = false;
+
+      foreach (var mod in MyAPIGateway.Session.Mods)
+      {
+        if (aqdVisualsPresent && emissiveColorsPresent)
+          break;
+
+        if (mod.PublishedFileId == 2244563617) // AQD - Visuals
+          aqdVisualsPresent = true;
+        else if (mod.PublishedFileId == 2212516940) // Emissive Colors - Red / Green Color Vision Deficiency
+          emissiveColorsPresent = true;
+      }
+
+      if (aqdVisualsPresent)
+      {
+        RED = new Color(171, 42, 29);
+        GREEN = emissiveColorsPresent ? new Color(10, 255, 25) : new Color(60, 163, 33);
+      }
+      else if (emissiveColorsPresent)
+      {
+        GREEN = new Color(10, 255, 25);
+      }
+
+      base.UpdateOnceBeforeFrame();
+    }
+
+    public override void Close()
+    {
+      m_block.IsWorkingChanged -= OnIsWorkingChanged;
+      m_block = null;
+      base.Close();
+    }
+
+    private void OnIsWorkingChanged(VRage.Game.ModAPI.IMyCubeBlock obj)
+    {
+      if (MyAPIGateway.Session == null)
+        return;
+
+      if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+        return;
+
+      var color = IsWorking ? GREEN : RED;
+      m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, color, 1f);
+    }
+  }
 }
