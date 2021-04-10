2a6ebb647e01d66416ac0b99d57725015c831c1d
enenra <enenra9@gmail.com>
2021-04-10 17:48:53 +0200

    Armor Expansion 1.5b - Register blocks with WC

---
+++ AQD - Armor Expansion/Content/Data/Scripts/WeaponThread/MasterConfig.cs
@@ -0,0 +1,95 @@
+  
+namespace WeaponThread
+{
+    partial class Weapons
+    {
+        internal Weapons()
+        {
+            // LG
+            LightArmorSubtypes("AQD_LG_LA_Corner_Split_2x1x1_Base", 
+            "AQD_LG_LA_Corner_Split_2x1x1_Tip", 
+            "AQD_LG_LA_HalfPlate", 
+            "AQD_LG_LA_Plate_1x1", 
+            "AQD_LG_LA_Plate_2x1Base", 
+            "AQD_LG_LA_Plate_2x1Tip", 
+            "AQD_LG_LA_Plate_Slope", 
+            "AQD_LG_LA_Plate_Slope2x1", 
+            "AQD_LG_LA_Plate_Triangle", 
+            "AQD_LG_LA_Slab_Corner_Split", 
+            "AQD_LG_LA_Slab_Half_Corner_Split", 
+            "AQD_LG_LA_Slab_InvCorner_Split", 
+            "AQD_LG_LA_Slab_RaisedCorner_Split", 
+            "AQD_LG_LA_Slope3x1", 
+            "AQD_LG_LA_Slope3x1_Corner", 
+            "AQD_LG_LA_Slope3x1_InvCorner", 
+            "AQD_LG_LA_Slope4x1", 
+            "AQD_LG_LA_Slope4x1_Corner", 
+            "AQD_LG_LA_Slope4x1_InvCorner"
+            );
+            // SG
+            LightArmorSubtypes("AQD_SG_LA_Corner_Split_2x1x1_Base", 
+            "AQD_SG_LA_Corner_Split_2x1x1_Tip", 
+            "AQD_SG_LA_HalfPlate", 
+            "AQD_SG_LA_Plate_1x1", 
+            "AQD_SG_LA_Plate_2x1Base", 
+            "AQD_SG_LA_Plate_2x1Tip", 
+            "AQD_SG_LA_Plate_Slope", 
+            "AQD_SG_LA_Plate_Slope2x1", 
+            "AQD_SG_LA_Plate_Triangle", 
+            "AQD_SG_LA_Slab_Corner_Split", 
+            "AQD_SG_LA_Slab_Half_Corner_Split", 
+            "AQD_SG_LA_Slab_InvCorner_Split", 
+            "AQD_SG_LA_Slab_RaisedCorner_Split", 
+            "AQD_SG_LA_Slope3x1", 
+            "AQD_SG_LA_Slope3x1_Corner", 
+            "AQD_SG_LA_Slope3x1_InvCorner", 
+            "AQD_SG_LA_Slope4x1", 
+            "AQD_SG_LA_Slope4x1_Corner", 
+            "AQD_SG_LA_Slope4x1_InvCorner"
+            );
+
+            // LG
+            HeavyArmorSubtypes("AQD_LG_HA_Corner_Split_2x1x1_Base", 
+            "AQD_LG_HA_Corner_Split_2x1x1_Tip", 
+            "AQD_LG_HA_HalfPlate", 
+            "AQD_LG_HA_Plate_1x1", 
+            "AQD_LG_HA_Plate_2x1Base", 
+            "AQD_LG_HA_Plate_2x1Tip", 
+            "AQD_LG_HA_Plate_Slope", 
+            "AQD_LG_HA_Plate_Slope2x1", 
+            "AQD_LG_HA_Plate_Triangle", 
+            "AQD_LG_HA_Slab_Corner_Split", 
+            "AQD_LG_HA_Slab_Half_Corner_Split", 
+            "AQD_LG_HA_Slab_InvCorner_Split", 
+            "AQD_LG_HA_Slab_RaisedCorner_Split", 
+            "AQD_LG_HA_Slope3x1", 
+            "AQD_LG_HA_Slope3x1_Corner", 
+            "AQD_LG_HA_Slope3x1_InvCorner", 
+            "AQD_LG_HA_Slope4x1", 
+            "AQD_LG_HA_Slope4x1_Corner", 
+            "AQD_LG_HA_Slope4x1_InvCorner"
+            );
+            // SG
+            HeavyArmorSubtypes("AQD_SG_HA_Corner_Split_2x1x1_Base", 
+            "AQD_SG_HA_Corner_Split_2x1x1_Tip", 
+            "AQD_SG_HA_HalfPlate", 
+            "AQD_SG_HA_Plate_1x1", 
+            "AQD_SG_HA_Plate_2x1Base", 
+            "AQD_SG_HA_Plate_2x1Tip", 
+            "AQD_SG_HA_Plate_Slope", 
+            "AQD_SG_HA_Plate_Slope2x1", 
+            "AQD_SG_HA_Plate_Triangle", 
+            "AQD_SG_HA_Slab_Corner_Split", 
+            "AQD_SG_HA_Slab_Half_Corner_Split", 
+            "AQD_SG_HA_Slab_InvCorner_Split", 
+            "AQD_SG_HA_Slab_RaisedCorner_Split", 
+            "AQD_SG_HA_Slope3x1", 
+            "AQD_SG_HA_Slope3x1_Corner", 
+            "AQD_SG_HA_Slope3x1_InvCorner", 
+            "AQD_SG_HA_Slope4x1", 
+            "AQD_SG_HA_Slope4x1_Corner", 
+            "AQD_SG_HA_Slope4x1_InvCorner"
+            );
+        }
+    }
+}
