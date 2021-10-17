04abf77b5075479a5370c03bfed714f1de7053d1
enenra <enenra9@gmail.com>
2021-10-17 22:29:55 +0200

    Reinforced Windows 1.1 - CoreParts armor defs for the window blocks

---
+++ AQD - Reinforced Windows/Content/Data/Scripts/CoreParts/Armor.cs
@@ -0,0 +1,29 @@
+using static Scripts.Structure;
+using static Scripts.Structure.ArmorDefinition.ArmorType;
+namespace Scripts {   
+    partial class Parts {
+        // Don't edit above this line
+        ArmorDefinition AQD_ReinforcedWindows => new ArmorDefinition
+        {
+            SubtypeIds = new[] {
+                "AQD_LG_ReinforcedWindow_1x1",
+                "AQD_SG_ReinforcedWindow_1x1",
+                "AQD_LG_ReinforcedWindow_1x1Corner",
+                "AQD_SG_ReinforcedWindow_1x1Corner",
+                "AQD_LG_ReinforcedWindow_1x1CornerInv",
+                "AQD_SG_ReinforcedWindow_1x1CornerInv",
+                "AQD_LG_ReinforcedWindow_1x1Slope",
+                "AQD_SG_ReinforcedWindow_1x1Slope",
+                "AQD_LG_ReinforcedWindow_1x2Corner",
+                "AQD_SG_ReinforcedWindow_1x2Corner",
+                "AQD_LG_ReinforcedWindow_1x2CornerInv",
+                "AQD_SG_ReinforcedWindow_1x2CornerInv",
+                "AQD_LG_ReinforcedWindow_1x2Slope",
+                "AQD_SG_ReinforcedWindow_1x2Slope"
+            },
+            EnergeticResistance = 1.0f, //Resistance to Energy damage. 0.5f = 200% damage, 2f = 50% damage
+            KineticResistance = 1.0f, //Resistance to Kinetic damage. Leave these as 1 for no effect
+            Kind = Heavy, //Heavy, Light, NonArmor - which ammo damage multipliers to apply
+        };
+    }
+}
