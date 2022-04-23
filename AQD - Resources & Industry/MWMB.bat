10937147b58f1b2155d610fab844480cae9b3c23
enenra <enenra9@gmail.com>
2022-04-23 21:12:22 +0200

    Upgradeable Gyroscopes 1.1b - Made Gyro Boosters airtight

--- AQD - Resources & Industry/MWMB.bat
+++
@@ -1,2 +0,0 @@
-"E:\Games\Modding\SE\tools\MwmBuilder\MwmBuilder.exe" /f /s:.\OriginalContent\ /o:.\Content\ /x:"C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineersModSDK\OriginalContent\Materials"
-PAUSE
