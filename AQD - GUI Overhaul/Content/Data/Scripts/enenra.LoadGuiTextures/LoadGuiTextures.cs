e18e7dcea1825cc96141640e2cd96ceec8512292
enenra <enenra9@gmail.com>
2023-01-21 16:34:02 +0100

    GUI Overhaul - In case this is ever possible

---
+++ AQD - GUI Overhaul/Content/Data/Scripts/enenra.LoadGuiTextures/LoadGuiTextures.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace enenra.LoadGuiTextures
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class LoadGuiTextures : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            foreach (MyDefinitionBase def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+                MyCubeBlockDefinition blockDef = def as MyCubeBlockDefinition;
+
+                if (blockDef == null)
+                    continue;
+
+                if (blockDef.BlockTopology == null)
+                    continue;
+
+                if (blockDef.CubeDefinition == null)
+                    continue;
+
+                if (blockDef.BlockTopology == MyBlockTopology.TriangleMesh)
+                    continue;
+
+                if (!blockDef.CubeDefinition.ShowEdges)
+                    continue;
+
+                blockDef.CubeDefinition.ShowEdges = false;
+                revertShowEdges.Add(blockDef);
+            }
+        }
+    }
+}
