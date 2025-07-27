a91f72d8f71a9f6dc086b5fb266cc3a5a154e269
enenra <enenra9@gmail.com>
2025-07-27 14:05:10 +0200

    Concrete - BlockVariantGroups

--- AQD - Concrete/OriginalContent/create_bvgs.py
+++ AQD - Concrete/OriginalContent/create_bvgs.py
@@ -73,6 +73,9 @@ def create_bvgs():
                                 else:
                                     entry_block = entry_block.replace("Large", f"AQD_LG_{id}_")
 
+                                if f"AQD_LG_{id}_" not in entry_block:
+                                    entry_block = f"AQD_LG_{id}_" + entry_block
+
                                 assignments[new_subtype].append(entry_block.replace("LargeBlock", "AQD_Concrete_"))
 
                             entry = entry[entry.find(" />") + len(" />"):]
@@ -80,99 +83,47 @@ def create_bvgs():
                     lines = lines[lines.find("</BlockVariantGroup>") + len("</BlockVariantGroup>"):]
                 wf.close()
 
-    print(assignments)
-    return
-
     defs = ET.Element('Definitions')
     defs.set('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
     defs.set('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema')
     bvgs = ET.SubElement(defs, 'BlockVariantGroups')
 
-    bvg_letters = ET.SubElement(bvgs, 'BlockVariantGroup')
-    bvg_numbers = ET.SubElement(bvgs, 'BlockVariantGroup')
-    bvg_symbols = ET.SubElement(bvgs, 'BlockVariantGroup')
+    for bvg_subtype, entry_blocks in assignments.items():
+        bvg = ET.SubElement(bvgs, 'BlockVariantGroup')
 
-    hdr_letters = False
-    hdr_numbers = False
-    hdr_symbols = False
-    for b in blocks:
-        if b.endswith(tuple(numbers)):
-            if not hdr_numbers:
-                id = ET.SubElement(bvg_numbers, 'Id')
-                id.set("Type", "MyObjectBuilder_BlockVariantGroup")
-                id.set("Subtype", f"{prefix}Numbers1")
+        id = ET.SubElement(bvg, 'Id')
+        id.set("Type", "MyObjectBuilder_BlockVariantGroup")
+        id.set("Subtype", bvg_subtype)
 
-                icon = ET.SubElement(bvg_numbers, 'Icon')
-                icon.text = f"Textures\GUI\Icons\Cubes\{prefix}LG_Symbol_0.dds"
+        icon = ET.SubElement(bvg, 'Icon')
+        icon.text = f"Textures\\GUI\\Icons\\Cubes\\{entry_blocks[0]}.dds"
 
-                dpname = ET.SubElement(bvg_numbers, 'DisplayName')
-                dpname.text = f"DisplayName_{prefix}LG_Symbol_0"
+        dpname = ET.SubElement(bvg, 'DisplayName')
+        dpname.text = f"DisplayName_{entry_blocks[0]}"
 
-                descr = ET.SubElement(bvg_numbers, 'Description')
-                descr.text = "Description_Numbers"
+        descr = ET.SubElement(bvg, 'Description')
+        descr.text = "Description_AQD_Concrete_Block"
 
-                n_entries = ET.SubElement(bvg_numbers, 'Blocks')
+        bvg_entries = ET.SubElement(bvg, 'Blocks')
 
-                hdr_numbers = True
-
-            entry = ET.SubElement(n_entries, 'Block')
+        for b in entry_blocks:
+            entry = ET.SubElement(bvg_entries, 'Block')
             entry.set("Type", "MyObjectBuilder_CubeBlock")
             entry.set("Subtype", b)
-
-        elif b.endswith(tuple(symbols)):
-            if not hdr_symbols:
-                id = ET.SubElement(bvg_symbols, 'Id')
-                id.set("Type", "MyObjectBuilder_BlockVariantGroup")
-                id.set("Subtype", f"{prefix}Symbols")
-
-                icon = ET.SubElement(bvg_symbols, 'Icon')
-                icon.text = f"Textures\GUI\Icons\Cubes\{prefix}LG_Symbol_And.dds"
-
-                dpname = ET.SubElement(bvg_symbols, 'DisplayName')
-                dpname.text = f"DisplayName_{prefix}LG_Symbol_And"
-
-                descr = ET.SubElement(bvg_symbols, 'Description')
-                descr.text = "Description_Symbols"
-
-                s_entries = ET.SubElement(bvg_symbols, 'Blocks')
-
-                hdr_symbols = True
-
-            entry = ET.SubElement(s_entries, 'Block')
-            entry.set("Type", "MyObjectBuilder_CubeBlock")
-            entry.set("Subtype", b)
-
-        else:
-            if not hdr_letters:
-                id = ET.SubElement(bvg_letters, 'Id')
-                id.set("Type", "MyObjectBuilder_BlockVariantGroup")
-                id.set("Subtype", f"{prefix}LettersAZ")
-
-                icon = ET.SubElement(bvg_letters, 'Icon')
-                icon.text = f"Textures\GUI\Icons\Cubes\{prefix}LG_Symbol_A.dds"
-
-                dpname = ET.SubElement(bvg_letters, 'DisplayName')
-                dpname.text = f"DisplayName_{prefix}LG_Symbol_A"
-
-                descr = ET.SubElement(bvg_letters, 'Description')
-                descr.text = "Description_Letters"
-
-                l_entries = ET.SubElement(bvg_letters, 'Blocks')
-
-                hdr_letters = True
-
-            entry = ET.SubElement(l_entries, 'Block')
-            entry.set("Type", "MyObjectBuilder_CubeBlock")
-            entry.set("Subtype", b)
-
 
     temp_string = ET.tostring(defs, 'utf-8')
     temp_string.decode('ascii')
     xml_string = xml.dom.minidom.parseString(temp_string)
     xml_formatted = xml_string.toprettyxml()
 
-    target_file = os.path.join(mod_path, 'Data', "BlockVariantGroups.sbc")
+    # Concrete
+    target_file = os.path.join(MOD_PATH, 'Data', "BlockVariantGroups_Concrete.sbc")
     exported_xml = open(target_file, "w")
     exported_xml.write(xml_formatted)
+
+    # ReinforcedConcrete
+    target_file = os.path.join(MOD_PATH, 'Data', "BlockVariantGroups_ReinforcedConcrete.sbc")
+    exported_xml = open(target_file, "w")
+    exported_xml.write(xml_formatted.replace("_Concrete_", "_ReinforcedConcrete_"))
 
 create_bvgs()
